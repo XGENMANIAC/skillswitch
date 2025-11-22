5195d6fb88c57cf4f82cb8e8061a5d42c40795b8
Nicodemus <nicodemusmuema76@gmail.com>
2025-11-22 18:32:58 +0000

    Auto-commit: Agent tool execution

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SkillShareApp from './SkillShareApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SkillShareApp />
+  </React.StrictMode>
+);
